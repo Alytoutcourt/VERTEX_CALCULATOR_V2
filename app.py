1b650fec3a7bd2e27bfb48c14e07dae79dae80e9
Aly <Aly.jalil@icloud.com>
2024-04-22 23:53:13 +0200

    etyj

--- app.py
+++ app.py
@@ -1,46 +1,16 @@
+import os
 import streamlit as st
-import ifcopenshell
 
-st.title("IFC File Operations")
+st.title("File Uploader")
 
-# Champ de saisie pour le chemin du fichier IFC
-ifc_filepath = st.text_input("Entrez le chemin du fichier IFC")
+# Charger le fichier
+uploaded_file = st.file_uploader("Uploader un fichier", type=["txt", "csv", "pdf"])
 
-# Vérifier si un chemin de fichier a été saisi
-if ifc_filepath:
-    try:
-        # Ouvrir le fichier IFC
-        model = ifcopenshell.open(ifc_filepath)
-
-        # Exemple : Obtenir tous les types de murs
-        st.subheader("Exemple : Obtenir tous les types de murs")
-        for wall_type in model.by_type("IfcWallType"):
-            st.write(f"Type de mur : {wall_type.Name}")
-
-        # Exemple : Obtenir tous les product
-        st.subheader("Exemple : Obtenir tous les ifcproduct")
-        for p in model.by_type("IfcProduct"):
-            st.write(f"Instance : {p.Name}")
-
-        # Exemple : Obtenir toutes les occurrences de portes d'un type
-        st.subheader("Exemple : Obtenir toutes les occurrences de portes d'un type")
-        for door_type in model.by_type("IfcDoorType"):
-            st.write(f"Type de porte : {door_type.Name}")
-            doors = ifcopenshell.util.element.get_types(door_type)
-            st.write(f"Il y a {len(doors)} portes de ce type.")
-            for door in doors:
-                st.write(f"Nom de la porte : {door.Name}")
-
-        # Exemple : Obtenir le type d'un mur
-        st.subheader("Exemple : Obtenir le type d'un mur")
-        wall = model.by_type("IfcWall")[0]
-        wall_type = ifcopenshell.util.element.get_type(wall)
-        st.write(f"Le type de mur de {wall.Name} est {wall_type.Name}")
-
-        # Exemple : Obtenir les propriétés d'un type de mur
-        st.subheader("Exemple : Obtenir les propriétés d'un type de mur")
-        psets = ifcopenshell.util.element.get_psets(wall_type)
-        st.write(psets)
-
-    except FileNotFoundError:
-        st.error("Fichier IFC introuvable. Veuillez vérifier le chemin du fichier.")
+# Vérifier si un fichier a été téléchargé
+if uploaded_file is not None:
+    # Enregistrer le fichier dans un répertoire temporaire
+    with open(os.path.join("temp_files", uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    # Obtenir le chemin absolu du fichier sur le serveur
+    file_path = os.path.abspath(os.path.join("temp_files", uploaded_file.name))
+    st.write(f"Le fichier a été téléchargé avec succès. Chemin du fichier sur le serveur : {file_path}")
