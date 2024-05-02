19950731186c8eee3dab8d620ef04cd736d4edd6
Aly <Aly.jalil@icloud.com>
2024-05-03 01:13:21 +0200

    infos ifc

--- app.py
+++ app.py
@@ -17,6 +17,29 @@ def main():
     # Convertit le contenu du fichier en objet IFC à l'aide de ifcopenshell
     ifc_file = ifcopenshell.file.from_string(uploaded_file.getvalue().decode("utf-8"))
 
+    #Récupérer les IfcProjetc,IfcSite,IfcBuilding et les IfcBuildingStorey et les affichier dans un DataFrame :
+        # Récupération des entités IfcProject
+    projects = ifc_file.by_type("IfcProject")
+    project_info = [(project.Name, project.GlobalId) for project in projects]
+
+    # Récupération des entités IfcSite
+    sites = ifc_file.by_type("IfcSite")
+    site_info = [(site.Name, site.GlobalId) for site in sites]
+
+    # Récupération des entités IfcBuilding
+    buildings = ifc_file.by_type("IfcBuilding")
+    building_info = [(building.Name, building.GlobalId) for building in buildings]
+
+    # Récupération des entités IfcBuildingStorey
+    building_storeys = ifc_file.by_type("IfcBuildingStorey")
+    building_storey_info = [(storey.Name, storey.GlobalId) for storey in building_storeys]
+
+    # Création d'un DataFrame pour toutes les informations
+    identity_df = pd.DataFrame(project_info + site_info + building_info + building_storey_info, columns=["Nom", "GUID"])
+
+    # Affichage du DataFrame
+    st.write("### Informations sur les entités dans le fichier IFC :")
+    st.dataframe(identity_df)
     # Configuration des paramètres pour la géométrie
     settings = ifcopenshell.geom.settings()
 
