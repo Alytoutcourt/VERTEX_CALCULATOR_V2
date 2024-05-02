a68e8dd8fb4d9eea7a69c9328b6b7bcaf5134d65
Aly <Aly.jalil@icloud.com>
2024-05-02 23:41:54 +0200

    working

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-import os
 import streamlit as st
+import ifcopenshell
+import ifcopenshell.geom
+import multiprocessing
 
-st.title("File Uploader")
+def main():
+    st.title("Informations géométriques dans un fichier IFC")
 
-# Charger le fichier
-uploaded_file = st.file_uploader("Uploader un fichier", type=["txt", "csv", "pdf"])
+    # Chargement du fichier IFC
+    uploaded_file = st.file_uploader("Uploader le fichier IFC", type=['ifc'])
+    if not uploaded_file:
+        st.warning("Veuillez uploader un fichier IFC.")
+        return
 
-# Vérifier si un fichier a été téléchargé
-if uploaded_file is not None:
-    # Enregistrer le fichier dans un répertoire temporaire
-    with open(os.path.join("temp_files", uploaded_file.name), "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    # Obtenir le chemin absolu du fichier sur le serveur
-    file_path = os.path.abspath(os.path.join("temp_files", uploaded_file.name))
-    st.write(f"Le fichier a été téléchargé avec succès. Chemin du fichier sur le serveur : {file_path}")
+    # Convertit le contenu du fichier en objet IFC à l'aide de ifcopenshell
+    ifc_file = ifcopenshell.file.from_string(uploaded_file.getvalue().decode("utf-8"))
+
+    # Configuration des paramètres pour la géométrie
+    settings = ifcopenshell.geom.settings()
+
+    # Création d'un itérateur pour extraire la géométrie
+    Element = ifc_file.by_type('IfcProduct')
+    iterator = ifcopenshell.geom.iterator(settings, ifc_file, multiprocessing.cpu_count(), include=Element)
+    # Initialisation de l'itérateur
+    if iterator.initialize():
+        verts_list = []
+
+        # Parcours de la géométrie
+        while True:
+            shape = iterator.get()
+            if shape is None:
+                break
+            
+            # Récupération des sommets
+            verts = shape.geometry.verts
+
+            # Ajout des informations à la liste respective
+            verts_list.extend(verts)
+
+            if not iterator.next():
+                break
+
+        # Affichage des informations dans un tableau
+        st.write("### Sommets")
+        st.write(len(verts_list))
+
+if __name__ == "__main__":
+    main()
